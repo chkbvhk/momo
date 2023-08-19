5f35deac082f6ab01ee080a082e8a58ba145ce0a
chkbvhk <85958677+chkbvhk@users.noreply.github.com>
2023-08-19 22:21:49 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -29,7 +29,7 @@ const MMSLS = [
    * @param {number} min 最小值（包含）
    * @param {number} max 最大值（不包含）
    */
-  function randomNumber(min = 0, max = 200) {
+  function randomNumber(min = 0, max = 100) {
     return Math.min(Math.floor(min + Math.random() * (max - min)), max);
   }
   const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
